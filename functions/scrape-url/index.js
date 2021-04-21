cc30f9204442ee70ccd83656dbec3fadd3b34d3a
Elihu Munguía <47308963+XVI-LoLz@users.noreply.github.com>
2021-04-21 14:04:56 -0500

    add: fontFace scraping logic**
    **add: "--disable-web-security" arg to read stylesheets correctly

--- functions/scrape-url/index.js
+++ functions/scrape-url/index.js
@@ -1,7 +1,12 @@
 const puppeteer = require("puppeteer");
 
 const options = {
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-web-security",
+  ],
 };
 
 async function scrapeUrl(url) {
@@ -11,13 +16,82 @@ async function scrapeUrl(url) {
 
   const page = await browser.newPage();
 
-  await page.goto(url).catch((e) => {
+  await page.goto(url, { waitUntil: "domcontentloaded" }).catch((e) => {
     console.error("Goto error: ", e);
     throw new Error("Invalid URL");
   });
 
   const data = await page.evaluate(() => {
     const separateColors = /(?<=\)) (?=r)/;
+
+    function parseFontsFromCSSString(css) {
+      const currentHost = window.location.host;
+      const fontFaceRegex = /\@font\-face\s?\{([\s\S]+?)\}/gm;
+      const fontFamilyRegex = /font\-family\:\s?(.*)/;
+      const urlRegex = /url\(['"]?(.+?)['"]?\)/g;
+
+      const matches = css.matchAll(fontFaceRegex);
+
+      if (!matches) {
+        return;
+      }
+
+      const results = {};
+
+      for (const match of matches) {
+        const rules = match[1];
+
+        // Split our declaration by line and by semicolon
+        const lines = rules
+          .split(/;\n?/)
+          .map((line) => line.trim())
+          .filter((v) => v);
+
+        // Find the font-family rule:
+        const familyLine = lines.find((line) => fontFamilyRegex.test(line));
+
+        if (!familyLine) {
+          continue;
+        }
+
+        const family = familyLine.match(fontFamilyRegex)[1];
+
+        const familyUrls = [];
+
+        // Extract the URLs from the raw lines
+        for (const line of lines) {
+          const urlMatches = line.matchAll(urlRegex);
+          if (!urlMatches) {
+            continue;
+          }
+          for (const urlMatch of urlMatches) {
+            let rawUrl = urlMatch[1];
+            // Add a host if one isn't specified
+            const absoluteUrl = new URL(rawUrl, document.baseURI).href;
+            familyUrls.push(absoluteUrl);
+          }
+        }
+
+        results[family] = familyUrls;
+      }
+
+      return results;
+    }
+
+    function getAllStyles() {
+      const styles = [];
+      for (const sheet of document.styleSheets) {
+        const rules = sheet?.cssRules || sheet?.rules;
+        for (const rule of rules) {
+          const style =
+            rule.cssText || `${rule.selectorText} { ${rule.style.cssText}`;
+          styles.push(style);
+        }
+      }
+
+      return styles;
+    }
+
     const elements = document.body.getElementsByTagName("*");
 
     const images = {};
@@ -25,6 +99,9 @@ async function scrapeUrl(url) {
     Array.from(document.images, (e) => e.src).forEach(
       (image) => (images[image] = (images[image] || 0) + 1)
     );
+
+    const input = getAllStyles().join("\n");
+    const fontFaces = parseFontsFromCSSString(input);
 
     const styles = {
       fonts: {},
@@ -65,6 +142,7 @@ async function scrapeUrl(url) {
     };
 
     return {
+      fontFaces,
       fonts: getOrdered(styles.fonts),
       colors: getOrdered(styles.colors),
       backgroundColors: getOrdered(styles.backgroundColors),
